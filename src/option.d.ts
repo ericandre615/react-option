2ec4d4d0648cd78738291df9e98f8efdec7e2e64
ericandre615 <ericandre615@gmail.com>
2021-06-23 18:05:27 -0500

    fix Option type to be also used as child

--- src/option.d.ts
+++ src/option.d.ts
@@ -1,22 +1,20 @@
 import React from 'react'
 
-// declare module 'react-option' {
-  export type OptionProps = {
-    children: OptionChildren;
-    value: boolean;
-  };
-  export type OptionChild = typeof Some | typeof None | typeof Option;
-  export type OptionChildren = OptionChild | OptionChild[];
-  type SomeProps = {
-    _optionType: '[Option: Some]';
-  }
+export type OptionProps = {
+  children: OptionChildren;
+  value: boolean;
+};
+export type OptionChild = typeof Some | typeof None | typeof Option;
+export type OptionChildren = OptionChild | OptionChild[];
+type SomeProps = {
+  _optionType: '[Option: Some]';
+}
 
-  type NoneProps = {
-    _optionType: '[Option: None]';
-  }
-  //function Some(props: SomeProps): React.ReactNode;
-  //function None(props: NoneProps): React.ReactNode;
-  export const Some: React.FC<SomeProps>;
-  export const None: React.FC<NoneProps>;
-  function Option(props: OptionProps): React.ReactNode;
-//}
+type NoneProps = {
+  _optionType: '[Option: None]';
+}
+
+export const Some: React.FC<SomeProps>;
+export const None: React.FC<NoneProps>;
+export const Option: React.FC<OptionProps>;
+
